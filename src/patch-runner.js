cf321f31df7fb5ad1320947ab1e505649d0d2374
Michael Rawlings <ml.rawlings@gmail.com>
2018-04-20 16:35:56 -0400

    don't do set up twice when --grep flag is used

--- src/patch-runner.js
+++ src/patch-runner.js
@@ -6,38 +6,40 @@ const grep = Runner.prototype.grep;
 // grep is called from the constructor
 // so this is additional construction code
 Runner.prototype.grep = function() {
-    this.tests = {
-        unexpectedPasses: [],
-        unexpectedFails: [],
-        expectedFails: [],
-        pending: []
+    if (!this.tests) {
+        this.tests = {
+            unexpectedPasses: [],
+            unexpectedFails: [],
+            expectedFails: [],
+            pending: []
+        }
+        this.on('pass', (test) => {
+            if (test.shouldFail) {
+                this.failures++;
+                if (this.stats) {
+                    this.stats.unexpectedPasses = this.stats.unexpectedPasses || 0;
+                    this.stats.unexpectedPasses++;
+                    this.stats.passes--;
+                }
+                this.tests.unexpectedPasses.push(test);
+            }
+        });
+        this.on('fail', (test, err) => {
+            if (test.shouldFail) {
+                this.failures--;
+                if (this.stats) {
+                    this.stats.expectedFailures = this.stats.expectedFailures || 0;
+                    this.stats.expectedFailures++;
+                    this.stats.failures--;
+                }
+                this.tests.expectedFails.push(test);
+            } else {
+                this.tests.unexpectedFails.push(test);
+            }
+        });
+        this.on('pending', (test) => {
+            this.tests.pending.push(test);
+        });
     }
-    this.on('pass', (test) => {
-        if (test.shouldFail) {
-            this.failures++;
-            if (this.stats) {
-                this.stats.unexpectedPasses = this.stats.unexpectedPasses || 0;
-                this.stats.unexpectedPasses++;
-                this.stats.passes--;
-            }
-            this.tests.unexpectedPasses.push(test);
-        }
-    });
-    this.on('fail', (test, err) => {
-        if (test.shouldFail) {
-            this.failures--;
-            if (this.stats) {
-                this.stats.expectedFailures = this.stats.expectedFailures || 0;
-                this.stats.expectedFailures++;
-                this.stats.failures--;
-            }
-            this.tests.expectedFails.push(test);
-        } else {
-            this.tests.unexpectedFails.push(test);
-        }
-    });
-    this.on('pending', (test) => {
-        this.tests.pending.push(test);
-    });
     return grep.apply(this, arguments);
 }
